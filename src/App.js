5ee06af72e9a05c860c81144c9b270791db287e3
Edmund Xin <mxinburritos@gmail.com>
2021-01-17 12:11:19 -0800

    404 page and redirect added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
-import './App.css';
-import Homepage from './components/Homepage/Homepage';
-import NewsAndEvents from './components/NewsAndEvents/NewsAndEvents'
-import Navbar from './components/Navbar/Navbar'
-import GettingInvolved from './components/GettingInvolved/GettingInvolved'
-import Resources from './components/Resources/Resources'
-import AboutUs from './components/AboutUs/AboutUs'
+import "./App.css";
+import Homepage from "./components/Homepage/Homepage";
+import NewsAndEvents from "./components/NewsAndEvents/NewsAndEvents";
+import Navbar from "./components/Navbar/Navbar";
+import GettingInvolved from "./components/GettingInvolved/GettingInvolved";
+import Resources from "./components/Resources/Resources";
+import AboutUs from "./components/AboutUs/AboutUs";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-		<Router>
-		  <Navbar />
-			<Switch>
-				<Route exact path="/">
-					<Homepage />
-				</Route>
-				<Route exact path='/GettingInvolved'>
-					<GettingInvolved />
-				</Route>
-				<Route exact path='/NewsAndEvents'>
-					<NewsAndEvents />
-				</Route>
-				<Route exact path='/Resources'>
-					<Resources />
-				</Route>
-				<Route exact path='/AboutUs'>
-					<AboutUs />
-				</Route>
-			</Switch>
-		</Router>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="/GettingInvolved">
+          <GettingInvolved />
+        </Route>
+        <Route exact path="/NewsAndEvents">
+          <NewsAndEvents />
+        </Route>
+        <Route exact path="/Resources">
+          <Resources />
+        </Route>
+        <Route exact path="/AboutUs">
+          <AboutUs />
+        </Route>
+        <Route path="/404" component={NotFound} />
+        <Redirect from="*" to="/404" />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,48 +1,55 @@
 import React, { useState } from "react";
 import { Menu } from "semantic-ui-react";
-import {NavLink} from 'react-router-dom'
+import { NavLink, Link } from "react-router-dom";
+
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
-	const [activeItem, setActiveItem] = useState("");
+  const [activeItem, setActiveItem] = useState("home");
 
-	const handleItemClick = (e, { name }) => setActiveItem(name);
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
-	return (
-		<Menu secondary>
-			<Menu.Menu position="right">
-				<Menu.Item
-          as={NavLink} to='/'
-					name="home"
-					active={activeItem === "home"}
-					onClick={handleItemClick}
-				/>
-				<Menu.Item
-          as={NavLink} to='/GettingInvolved'
-					name="how to get involved"
-					active={activeItem === "how to get involved"}
-					onClick={handleItemClick}
-				/>
-				<Menu.Item
-          as={NavLink} to='/NewsAndEvents'
-					name="news and events"
-					active={activeItem === "news and events"}
-					onClick={handleItemClick}
-				/>
-				<Menu.Item
-          as={NavLink} to='/Resources'
-					name="resources"
-					active={activeItem === "resources"}
-					onClick={handleItemClick}
-				/>
-				<Menu.Item
-          as={NavLink} to='/AboutUs'
-					name="about us"
-					active={activeItem === "about us"}
-					onClick={handleItemClick}
-				/>
-			</Menu.Menu>
-		</Menu>
-	);
+  return (
+    <Menu secondary className={styles.links}>
+      <Menu.Menu position="right">
+        <Menu.Item
+          as={Link}
+          to="/"
+          name="home"
+          active={activeItem === "home"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          as={Link}
+          to="/GettingInvolved"
+          name="how to get involved"
+          active={activeItem === "how to get involved"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          as={Link}
+          to="/NewsAndEvents"
+          name="news and events"
+          active={activeItem === "news and events"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          as={Link}
+          to="/Resources"
+          name="resources"
+          active={activeItem === "resources"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          as={Link}
+          to="/AboutUs"
+          name="about us"
+          active={activeItem === "about us"}
+          onClick={handleItemClick}
+        />
+      </Menu.Menu>
+    </Menu>
+  );
 };
 
 export default Navbar;

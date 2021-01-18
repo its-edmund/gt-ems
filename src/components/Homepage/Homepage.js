e68bb698b26907c8da7445baa4131b92077d6038
Edmund Xin <mxinburritos@gmail.com>
2021-01-17 17:48:56 -0800

    landing sorta done

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Button } from "semantic-ui-react";
 
 import styles from "./Homepage.module.css";
+import landingpic from "./landing_page_image.png";
 
 const Homepage = () => {
-    return <h1 className={styles.title}>lorem Ipsum</h1>;
+  return (
+    <>
+      <div className={styles.landing}>
+        <div className={styles.titletext}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          <h3 className={styles.subtitle}>Praesent congue sodales magna, vitae tincidunt tortor vehicula a. Ut ut velit.</h3>
+          <Button className={styles.button}>Sed in.</Button>
+        </div>
+        <img
+          className={styles.landingimg}
+          src={landingpic}
+          alt="Medical Image"
+        />
+      </div>
+      <div className={styles.require}>
+        <h2>Require Assistance?</h2>
+      </div>
+    </>
+  );
 };
 
 export default Homepage;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,61 +1,56 @@
 import React, { useState } from "react";
-import classnames from 'classnames';
 import { Menu } from "semantic-ui-react";
-import { NavLink, Link } from "react-router-dom";
+import {NavLink} from 'react-router-dom'
+import classnames from 'classnames'
 
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
-  const [activeItem, setActiveItem] = useState("home");
+	const [activeItem, setActiveItem] = useState("home");
 
-  const handleItemClick = (e, { name }) => setActiveItem(name);
+	const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
-    <Menu secondary className={styles.links}>
-      <Menu.Menu position="right">
-        <Menu.Item
-          className={classnames(styles.link, styles.Change, activeItem === "home" ? styles.active : '')}
-          as={Link}
-          to="/"
-          name="home"
-          active={activeItem === "home"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          className={classnames(styles.link, styles.Change, activeItem === "how to get involved" ? styles.active : '')}
-          as={Link}
-          to="/GettingInvolved"
-          name="how to get involved"
-          active={activeItem === "how to get involved"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          className={classnames(styles.link, styles.Change, activeItem === "news and events" ? styles.active : '')}
-          as={Link}
-          to="/NewsAndEvents"
-          name="news and events"
-          active={activeItem === "news and events"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          className={classnames(styles.link, styles.Change, activeItem === "resources" ? styles.active : '')}
-          as={Link}
-          to="/Resources"
-          name="resources"
-          active={activeItem === "resources"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          className={classnames(styles.link, styles.Change, activeItem === "about us" ? styles.active : '')}
-          as={Link}
-          to="/AboutUs"
-          name="about us"
-          active={activeItem === "about us"}
-          onClick={handleItemClick}
-        />
-      </Menu.Menu>
-    </Menu>
-  );
+	return (
+		<Menu className={styles.links} secondary>
+			<Menu.Menu position="right">
+				<Menu.Item
+          className={classnames(styles.link, styles.Change, activeItem === "home" ? styles.active : "")}
+          as={NavLink} to='/'
+					name="home"
+					active={activeItem === "home"}
+					onClick={handleItemClick}
+				/>
+				<Menu.Item
+          className={classnames(styles.link, styles.Change, activeItem === "how to get involved" ? styles.active : "")}
+          as={NavLink} to='/GettingInvolved'
+					name="how to get involved"
+					active={activeItem === "how to get involved"}
+					onClick={handleItemClick}
+				/>
+				<Menu.Item
+          className={classnames(styles.link, styles.Change, activeItem === "news and events" ? styles.active : "")}
+          as={NavLink} to='/NewsAndEvents'
+					name="news and events"
+					active={activeItem === "news and events"}
+					onClick={handleItemClick}
+				/>
+				<Menu.Item
+          className={classnames(styles.link, styles.Change, activeItem === "resources" ? styles.active : "")}
+          as={NavLink} to='/Resources'
+					name="resources"
+					active={activeItem === "resources"}
+					onClick={handleItemClick}
+				/>
+				<Menu.Item
+          className={classnames(styles.link, styles.Change, activeItem === "about us" ? styles.active : "")}
+          as={NavLink} to='/AboutUs'
+					name="about us"
+					active={activeItem === "about us"}
+					onClick={handleItemClick}
+				/>
+			</Menu.Menu>
+		</Menu>
+	);
 };
 
 export default Navbar;

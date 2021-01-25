dd4051f0e1460411b37295d0f88c9dccb86b64c8
Edmund Xin <mxinburritos@gmail.com>
2021-01-24 21:19:52 -0800

    404 renabled

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom';
 
-import styles from "./App.module.css";
-import Homepage from "./components/Homepage/Homepage";
-import NewsAndEvents from "./components/NewsAndEvents/NewsAndEvents";
-import Navbar from "./components/Navbar/Navbar";
-import GettingInvolved from "./components/GettingInvolved/GettingInvolved";
-import Resources from "./components/Resources/Resources";
-import AboutUs from "./components/AboutUs/AboutUs";
-import NotFound from "./components/NotFound/NotFound";
-import Footer from './components/Footer/Footer'
+import styles from './App.module.css';
+import Homepage from './components/Homepage/Homepage';
+import NewsAndEvents from './components/NewsAndEvents/NewsAndEvents';
+import Navbar from './components/Navbar/Navbar';
+import GettingInvolved from './components/GettingInvolved/GettingInvolved';
+import Resources from './components/Resources/Resources';
+import AboutUs from './components/AboutUs/AboutUs';
+import NotFound from './components/NotFound/NotFound';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route exact path="/GettingInvolved">
-          <GettingInvolved />
-        </Route>
-        <Route exact path="/NewsAndEvents">
-          <NewsAndEvents />
-        </Route>
-        <Route exact path="/Resources">
-          <Resources />
-        </Route>
-        <Route exact path="/AboutUs">
-          <AboutUs />
-        </Route>
-        {/*<Route path="/404" component={NotFound} />
-        <Redirect from="*" to="/404" />*/}
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path='/'>
+                    <Homepage />
+                </Route>
+                <Route exact path='/GettingInvolved'>
+                    <GettingInvolved />
+                </Route>
+                <Route exact path='/NewsAndEvents'>
+                    <NewsAndEvents />
+                </Route>
+                <Route exact path='/Resources'>
+                    <Resources />
+                </Route>
+                <Route exact path='/AboutUs'>
+                    <AboutUs />
+                </Route>
+                <Route path='/404' component={NotFound} />
+                <Redirect from='*' to='/404' />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;

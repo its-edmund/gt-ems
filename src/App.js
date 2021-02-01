31debc9b28ca4e186dda377fdcbac552c4a56f7b
Edmund Xin <mxinburritos@gmail.com>
2021-02-01 13:50:25 -0800

    fix: suppress warnings

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import {
     BrowserRouter as Router,
@@ -15,6 +16,18 @@ import Resources from './components/Resources/Resources';
 import AboutUs from './components/AboutUs/AboutUs';
 import NotFound from './components/NotFound/NotFound';
 import Footer from './components/Footer/Footer';
+=======
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Homepage from "./components/Homepage/Homepage";
+import NewsAndEvents from "./components/NewsAndEvents/NewsAndEvents";
+import Navbar from "./components/Navbar/Navbar";
+import GettingInvolved from "./components/GettingInvolved/GettingInvolved";
+import Resources from "./components/Resources/Resources";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Footer from './components/Footer/Footer'
+>>>>>>> 61f0186 (fix: suppress warnings)
 
 function App() {
   return (

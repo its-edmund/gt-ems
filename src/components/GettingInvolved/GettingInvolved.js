0b7ad5a6ca07922c33b9f415280e453b1371ef88
Edmund Xin <mxinburritos@gmail.com>
2021-02-01 12:18:38 -0800

    initiatives finished roles started

--- src/components/GettingInvolved/GettingInvolved.js
+++ src/components/GettingInvolved/GettingInvolved.js
@@ -4,6 +4,37 @@ import { Button, Card } from 'semantic-ui-react';
 import styles from './GettingInvolved.module.css'
 import emergency from '../../assets/gt-emergency.png'
 
+const data = [
+	{
+		"title": "GT Emergency Announcements",
+		"body": "Phasellus et lectus ac arcu mollis elementum id malesuada urna. Etiam pulvinar magna ut auctor dignissim. Maecenas non nulla vehicula, molestie lorem id, vestibulum lacus.",
+		"hyperlink": "",
+		"image": emergency
+	},
+	{
+		"title": "Stamps Health Care",
+		"body": "Duis eu sollicitudin eros. Donec aliquam dolor eget rutrum fringilla. Fusce tempor lectus et ornare auctor. Duis mi ipsum, congue.",
+		"hyperlink": "",
+		"image": emergency
+	},
+	{
+		"title": "American Red Cross",
+		"body": "Maecenas mauris nunc, porttitor et lacinia in, varius vel tortor. Fusce vel dictum nisl, et convallis velit. Nulla fringilla, risus.",
+		"hyperlink": "",
+		"image": emergency
+	},
+	{
+		"title": "Emergency Medical Services",
+		"body": "In nec consequat ligula. Aliquam faucibus velit nunc, non viverra eros mattis et. Suspendisse potenti. Praesent pharetra, est in dignissim.",
+		"hyperlink": "",
+		"image": emergency
+	},
+]
+
+const roles = [
+
+]
+
 const GettingInvolved = () => {
 	return (
 		<>
@@ -12,17 +43,25 @@ const GettingInvolved = () => {
 				<h2 className={styles.jumbotron_subtitle}>Praesent congue sodales magna, vitae tincidunt tortor vehicula a. Ut ut velit. Praesent congue sodales magna, vitae tincidunt tortor vehicula a. Ut ut velit.</h2>
 				<Button className={styles.jumbotron_button}>Sed In.</Button>
 			</div>
-			<h2 className={styles.learnmore}>Learning more about our initiatives</h2>
+			<h2 className={styles.open_roles_header}>Open roles</h2>
+			<div className={styles.open_roles}>
+				{roles.length === 0 ? <div className={styles.no_roles}>There aren't any roles right now. Check back later!</div> : roles.map((role) => {
+
+				})}
+			</div>
+			<h2 className={styles.learnmore}>Learning more about our initiatives ✅</h2>
 			<div className={styles.initiatives}>
-				<div className={styles.initiative}>
-					<div className={styles.initiative_text}>
-						<h2 className={styles.initiative_title}>Initiative 1</h2>
-						<p className={styles.initiative_body}>Praesent congue sodales magna, vitae tincidunt tortor vehicula a. Ut ut velit. Praesent congue sodales magna, vitae tincidunt tortor vehicula a. Ut ut velit.</p>
+				{data.map((initiative, i) => (
+					<div className={styles.initiative}>
+						<div className={styles.initiative_text}>
+							<h2 className={styles.initiative_title}>{"Initiative " + (i + 1) + ": " + initiative.title}</h2>
+							<p className={styles.initiative_body}>{initiative.body}</p>
+						</div>
+						<div className={styles.initiative_image} >
+							<img className={styles.emergency} src={initiative.image} />
+						</div>
 					</div>
-					<div className={styles.initiative_image} >
-						<img className={styles.emergency} src={emergency} />
-					</div>
-				</div>
+				))}
 			</div>
 		</>
 	)

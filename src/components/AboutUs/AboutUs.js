f7a24145dca99b01e277b28d0f3772eb24790b81
Enzo Cabaltica <enzocollegemail@gmail.com>
2021-02-06 14:34:29 -0800

    about us merge resolved

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,15 +1,46 @@
 import React from "react";
 
-import "./AboutUs.css"
+import styles from "./AboutUs.module.css";
 
 const AboutUs = () => {
-	return (
-		<>
-			<h1 className="title">About Us</h1>
-			<h3>Who Are We?</h3>
-			<p>Fusce ullamcorper porttitor orci non malesuada. Nunc molestie metus in aliquam vulputate. Phasellus tempor mi at dui mattis, non pellentesque nunc lacinia. Morbi feugiat, mauris quis congue blandit, eros odio varius sapien, vitae hendrerit dui nibh in leo. Vivamus luctus, nibh id finibus blandit, turpis nisi elementum felis, vel pharetra orci massa ac leo. Vestibulum tempor urna ligula, id interdum felis laoreet eu. Curabitur facilisis elit non mi molestie, in suscipit sem mollis.</p>
-		</>
-	)
+  return (
+    <div className={styles.container}>
+      <h1>About Us</h1>
+      <div>
+        <h3>Who Are We</h3>
+        <p>
+          Fusce ullamcorper porttitor orci non malesuada. Nunc molestie metus in
+          aliquam vulputate. Phasellus tempor mi at dui mattis, non pellentesque
+          nunc lacinia. Morbi feugiat, mauris quis congue blandit, eros odio
+          varius sapien, vitae hendrerit dui nibh in leo. Vivamus luctus, nibh
+          id finibus blandit, turpis nisi elementum felis, vel pharetra orci
+          massa ac leo. Vestibulum tempor urna ligula, id interdum felis laoreet
+          eu. Curabitur facilisis elit non mi molestie, in suscipit sem mollis.
+        </p>
+        <h3>Our Mission</h3>
+        <p>
+          Suspendisse dictum malesuada augue. Etiam ut facilisis ante. Nunc
+          venenatis justo quis suscipit luctus. Vestibulum fermentum ornare
+          tellus, facilisis porttitor mi. Pellentesque pretium lacinia mi id
+          consectetur. Cras tempus est ut blandit auctor. Ut eros leo, mattis
+          fringilla leo sed, suscipit congue ipsum. Curabitur urna velit,
+          viverra id lectus in, dignissim elementum enim. Sed luctus vulputate
+          justo eget viverra. Etiam rutrum velit et arcu lobortis, finibus
+          feugiat risus suscipit. Mauris tincidunt aliquet ex, nec finibus urna
+          convallis sit amet. Ut vel leo eu turpis varius iaculis quis non ex.
+          In sit amet odio ut est finibus mattis. Suspendisse pellentesque
+          tortor.
+        </p>
+        <h3>Our Mission</h3>
+        <p>
+          Vivamus suscipit leo sed mi tempus, in eleifend diam varius. Nulla
+          molestie iaculis tristique. Morbi maximus consequat velit a pulvinar.
+          Sed blandit dignissim orci. Nunc rhoncus malesuada lectus. Fusce nec
+          posuere sem. Donec faucibus sapien at orci vehicula maximus.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;

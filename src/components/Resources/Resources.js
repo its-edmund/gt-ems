857e57d91239050c38af98f2852c31532be7d214
Edmund Xin <mxinburritos@gmail.com>
2021-02-06 23:54:42 -0800

    add video to resources

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import ReactPlayer from "react-player"
+
+import styles from './Resources.module.css'
 
 const Resources = () => {
-	return <h1>Resources</h1>
+	return (
+		<div className={styles.container}>
+			<h1>Resources</h1>
+			<div className={styles.video}>
+				<ReactPlayer url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+			</div>
+		</div>
+	)
 }
 
 export default Resources;

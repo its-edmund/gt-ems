83a49381bdcf951d672cf023bbc80000b2a863e2
Enzo Cabaltica <enzocollegemail@gmail.com>
2021-02-06 16:18:36 -0800

    finish about page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 import styles from "./AboutUs.module.css";
+import femaleFace from "../../assets/facefemale.png";
+import maleFace1 from "../../assets/faceMale.png";
+import maleFace2 from "../../assets/faceBeard.png";
+import { StayCurrentLandscapeSharp } from "@material-ui/icons";
 
 const AboutUs = () => {
   return (
     <div className={styles.container}>
-      <h1>About Us</h1>
-      <div>
-        <h3>Who Are We</h3>
-        <p>
+      <h1 className={styles.title}>About Us</h1>
+      <div className={styles.subcontainer}>
+        <h3 className={styles.header}>Who Are We</h3>
+        <p className={styles.body}>
           Fusce ullamcorper porttitor orci non malesuada. Nunc molestie metus in
           aliquam vulputate. Phasellus tempor mi at dui mattis, non pellentesque
           nunc lacinia. Morbi feugiat, mauris quis congue blandit, eros odio
@@ -17,8 +22,8 @@ const AboutUs = () => {
           massa ac leo. Vestibulum tempor urna ligula, id interdum felis laoreet
           eu. Curabitur facilisis elit non mi molestie, in suscipit sem mollis.
         </p>
-        <h3>Our Mission</h3>
-        <p>
+        <h3 className={styles.header}>Our Mission</h3>
+        <p className={styles.body}>
           Suspendisse dictum malesuada augue. Etiam ut facilisis ante. Nunc
           venenatis justo quis suscipit luctus. Vestibulum fermentum ornare
           tellus, facilisis porttitor mi. Pellentesque pretium lacinia mi id
@@ -31,14 +36,38 @@ const AboutUs = () => {
           In sit amet odio ut est finibus mattis. Suspendisse pellentesque
           tortor.
         </p>
-        <h3>Our Mission</h3>
-        <p>
+        <h3 className={styles.header}>Our Mission</h3>
+        <p className={styles.body}>
           Vivamus suscipit leo sed mi tempus, in eleifend diam varius. Nulla
           molestie iaculis tristique. Morbi maximus consequat velit a pulvinar.
           Sed blandit dignissim orci. Nunc rhoncus malesuada lectus. Fusce nec
           posuere sem. Donec faucibus sapien at orci vehicula maximus.
         </p>
       </div>
+      <div className={styles.profiles}>
+        <div className={styles.profile}>
+          <img className={styles.faces} src={femaleFace} alt="Female Face" />
+          <div>
+            <h4 className={styles.profile_header}>President</h4>
+            <p className={styles.profile_name}>Linda Smith</p>
+          </div>
+        </div>
+        <div className={styles.profile}>
+          <img className={styles.faces} src={maleFace1} alt="Male Face 1" />
+          <div>
+            <h4 className={styles.profile_header}>Vice President</h4>
+            <p className={styles.profile_name}>John Klein</p>
+          </div>
+        </div>
+        <div className={styles.profile}>
+          <img className={styles.faces} src={maleFace2} alt="Male Face 2" />
+          <div>
+            <h4 className={styles.profile_header}>Secretary</h4>
+            <p className={styles.profile_name}>Ryan Lopez</p>
+          </div>
+        </div>
+      </div>
+      <Button className={styles.button}>Join The Team!</Button>
     </div>
   );
 };

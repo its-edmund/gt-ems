b3f3a13cd16d33f2f5e62267b665f4acea488909
Edmund Xin <mxinburritos@gmail.com>
2021-02-14 02:32:02 -0800

    fix: remove contentful api key

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import { ApolloProvider, ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
 import "./index.css";
 import App from "./App";
-import "semantic-ui-css/semantic.min.css";
+
+const client = new ApolloClient({
+	uri: `https://graphql.contentful.com/content/v1/spaces/${REACT_APP_CONTENTFUL_SPACEID}`,
+	headers: {
+		Authorization: `Bearer ${process.env.REACT_APP_CONTENTFUL_TOKEN}`,
+	},
+	cache: new InMemoryCache()
+})
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );

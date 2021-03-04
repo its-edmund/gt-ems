3fa2d28b7f4a9562406b66f0b53a39299b05fa8b
Edmund Xin <mxinburritos@gmail.com>
2021-03-04 00:40:33 -0800

    feat: footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { Flex, Link } from '@chakra-ui/react';
+import { Stack, Flex, Link, Image, Box } from '@chakra-ui/react';
+import pic from '../../assets/gtems.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faSlack, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+// import { Flex, Link } from '@chakra-ui/react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faFacebookF, faInstagram, faLinkedin, faSlack } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <Flex w="100%" bgColor="mint.300" mt={40} p={30}>
+    <Flex w="100%" bgColor="mint.300" mt={40} p={30} flexDirection={'column'}>
+      <Flex mx='auto' flexDirection='row'>
+        <Flex flexDirection='column' mb={10} mr={10}>
+          <Box w='200px'>
+            <Link to='/'>
+              <Image src={pic}/>
+            </Link>
+          </Box>
+          <Stack direction='row' ml={5} spacing={3} color="mint.700">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <FontAwesomeIcon icon={faSlack} size="2x" />
+          </Stack>
+        </Flex>
+        <Stack direction="column" mt={2} spacing={1} w='100px' color='mint.700' fontSize={15}>
+          <Link href="/support">
+              Support
+          </Link>
+          <Link href="/contact">
+              Contact
+          </Link>
+          <Link href="/faq">
+              FAQ
+          </Link>
+          <Link href="/privacypolicy">
+              Privacy Policy
+          </Link>
+        </Stack>
+      </Flex>
       <Link
         target="_blank"
         href="https://dscgt.club"

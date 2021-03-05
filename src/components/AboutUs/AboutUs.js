4deb3a1ce4cc02962dd8f41de996df49902dc57c
Sai Enduri <enduri.sai@gmail.com>
2021-03-05 15:58:50 -0800

    refactor about us chakra ui

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
 import { useQuery, gql } from '@apollo/client';
-
-import styles from './AboutUs.module.css';
+import { Flex, Heading, Stack, Text, Link, Button, SimpleGrid, Box, Image } from '@chakra-ui/react';
 import Layout from '../Layout';
 
 const QUERY = gql`
@@ -34,39 +32,118 @@ const QUERY = gql`
 `;
 
 const AboutUs = () => {
-  const { data, loading } = useQuery(QUERY);
+  const { data, errors, loading } = useQuery(QUERY);
 
-  return (
+  console.log(errors);
+
+  return loading ? (
+    <div></div>
+  ) : (
     <Layout>
-      <div className={styles.container}>
-        <h1 className={styles.title}>About Us</h1>
-        {loading ? (
-          <div className={styles.no_roles}>Loading Data...</div>
-        ) : (
-          data.aboutUsTextCollection.items.map((section, i) => (
-            <div className={styles.subcontainer} key={i}>
-              <h3 className={styles.header}>{section.title}</h3>
-              <p className={styles.body}>{section.paragraph}</p>
-            </div>
-          ))
-        )}
-        <div className={styles.profiles}>
-          {loading ? (
-            <div className={styles.no_roles}>Loading Data...</div>
-          ) : (
-            data.personCollection.items.map((person, i) => (
-              <div className={styles.profile} key={i}>
-                <img alt="person face" className={styles.faces} src={person.profilePicture.url} />
-                <div>
-                  <h4 className={styles.profile_header}>{person.position}</h4>
-                  <p className={styles.profile_name}>{person.name}</p>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-        <Button className={styles.button}>Join The Team!</Button>
-      </div>
+      <Flex
+        align="center"
+        justify={{
+          base: 'center',
+          md: 'space-around',
+          xl: 'space-between',
+        }}
+        direction={{
+          base: 'column',
+          md: 'column',
+        }}
+        wrap="no-wrap"
+        minH="auto"
+        px={{ base: 8, md: 40 }}
+        mb={{ base: 8, md: 50 }}
+        height="container.sm"
+      >
+        <Heading
+          as="h1"
+          fontSize={{ base: '3xl', md: '4xl', xl: '5xl' }}
+          fontWeight="bold"
+          color="mint.700"
+          textAlign="center"
+          paddingBottom={50}
+        >
+          {'About Us'}
+        </Heading>
+        {data.aboutUsTextCollection.items.map((section) => (
+          <Stack spacing={4} w={{ base: '60%' }} align="center" key={section} paddingTop={5}>
+            <Heading
+              as="h1"
+              size="lg"
+              color="black"
+              opacity="0.8"
+              fontWeight="bold"
+              lineHeight={1.5}
+              textAlign={['center', 'center', 'left', 'left']}
+            >
+              {section.title}
+            </Heading>
+            <Text color="gray.500" fontSize="2xl">
+              {section.paragraph}
+            </Text>
+          </Stack>
+        ))}
+      </Flex>
+      <Flex
+        align="center"
+        justify={{
+          base: 'space-between',
+          md: 'space-around',
+          xl: 'space-between',
+        }}
+        direction={{
+          base: 'column',
+          md: 'column',
+        }}
+        px={{ base: 0, md: 10 }}
+      >
+        <SimpleGrid 
+          columns={3}
+          spacing={12}
+          w={{ base: '100%', md: '60%'}}
+          paddingTop={[60, 80, 100]}
+          justify={{
+            base: 'center',
+            md: 'space-around',
+            xl: 'space-between',
+          }}
+        >
+          {data.personCollection.items.map((person) => (
+            <Box w={{ base: '100%', sm: '100%', md: '100%' }} mb={{ base: 12, md: 0 }} key={person}>
+              <Image src={person.profilePicture.url} borderRadius="full" paddingLeft={3}/>
+              <Heading paddingLeft={10}>
+                {person.position}
+              </Heading>
+              <Text color="gray.500" fontSize="2xl" paddingLeft={10}>
+                {person.name}
+              </Text>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Flex>
+      <Stack
+        spacing={4}
+        w={{ base: '200%', md: '100%' }}
+        align='center'
+        paddingTop={20}
+      >
+        <Link textDecoration="none" href="/gettinginvolved">
+          <Button
+            backgroundColor="mint.700"
+            color="white"
+            borderRadius="8px"
+            py="4"
+            px="4"
+            lineHeight="1"
+            size="lg"
+            _hover={{ bg: 'mint.300' }}
+          >
+            Join our Team →
+          </Button>
+        </Link>
+      </Stack>
     </Layout>
   );
 };

f7982955a4e6f0e29a27c768c0cd493f1a5fb777
Sai Enduri <enduri.sai@gmail.com>
2021-03-05 18:54:21 -0800

    work on about us styling

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -54,37 +54,43 @@ const AboutUs = () => {
         wrap="no-wrap"
         minH="auto"
         px={{ base: 8, md: 40 }}
-        mb={{ base: 8, md: 50 }}
-        height="container.sm"
+        mb={{ base: 8, md: 10 }}
+        //height="container.sm"
       >
-        <Heading
-          as="h1"
-          fontSize={{ base: '3xl', md: '4xl', xl: '5xl' }}
-          fontWeight="bold"
-          color="mint.700"
-          textAlign="center"
-          paddingBottom={50}
+        <Stack
+          spacing={4}
+          w={{ base: '80%', md: '100%' }}
+          align={['center', 'center', 'center', 'center']}
         >
-          {'About Us'}
-        </Heading>
-        {data.aboutUsTextCollection.items.map((section) => (
-          <Stack spacing={4} w={{ base: '60%' }} align="center" key={section} paddingTop={5}>
-            <Heading
-              as="h1"
-              size="lg"
-              color="black"
-              opacity="0.8"
-              fontWeight="bold"
-              lineHeight={1.5}
-              textAlign={['center', 'center', 'left', 'left']}
-            >
-              {section.title}
-            </Heading>
-            <Text color="gray.500" fontSize="2xl">
-              {section.paragraph}
-            </Text>
-          </Stack>
-        ))}
+          <Heading
+            as="h1"
+            fontSize={{ base: '3xl', md: '4xl', xl: '5xl' }}
+            fontWeight="bold"
+            color="mint.700"
+            textAlign="center"
+            paddingBottom={[5, 5, 5]}
+          >
+            {'About Us'}
+          </Heading>
+          {data.aboutUsTextCollection.items.map((section) => (
+            <Stack spacing={4} w={{ base: '60%' }} align="center" key={section}>
+              <Heading
+                as="h1"
+                size="lg"
+                color="black"
+                opacity="0.8"
+                fontWeight="bold"
+                lineHeight={1.5}
+                textAlign={['center', 'center', 'left', 'left']}
+              >
+                {section.title}
+              </Heading>
+              <Text color="gray.500" fontSize="2xl">
+                {section.paragraph}
+              </Text>
+            </Stack>
+          ))}
+        </Stack>
       </Flex>
       <Flex
         align="center"
@@ -98,52 +104,53 @@ const AboutUs = () => {
           md: 'column',
         }}
         px={{ base: 0, md: 10 }}
+        mb={{ base: 8, md: 10 }}
+        wrap="no-wrap"
+        minH="auto"
       >
-        <SimpleGrid 
-          columns={3}
-          spacing={12}
-          w={{ base: '100%', md: '60%'}}
-          paddingTop={[60, 80, 100]}
-          justify={{
-            base: 'center',
-            md: 'space-around',
-            xl: 'space-between',
-          }}
+        <Stack
+          spacing={20}
+          w={{ base: '80%', md: '100%' }}
+          align={['center', 'center', 'center', 'center']}
         >
-          {data.personCollection.items.map((person) => (
-            <Box w={{ base: '100%', sm: '100%', md: '100%' }} mb={{ base: 12, md: 0 }} key={person}>
-              <Image src={person.profilePicture.url} borderRadius="full" paddingLeft={3}/>
-              <Heading paddingLeft={10}>
-                {person.position}
-              </Heading>
-              <Text color="gray.500" fontSize="2xl" paddingLeft={10}>
-                {person.name}
-              </Text>
-            </Box>
-          ))}
-        </SimpleGrid>
-      </Flex>
-      <Stack
-        spacing={4}
-        w={{ base: '200%', md: '100%' }}
-        align='center'
-        paddingTop={20}
-      >
-        <Link textDecoration="none" href="/gettinginvolved">
-          <Button
-            backgroundColor="mint.700"
-            color="white"
-            borderRadius="8px"
-            py="4"
-            px="4"
-            lineHeight="1"
-            size="lg"
-            _hover={{ bg: 'mint.300' }}
+          <SimpleGrid 
+            columns={3}
+            spacing={[4,6,12]}
+            w={{ base: '60%'}}
+            justify={{
+              base: 'center',
+              md: 'space-around',
+              xl: 'space-between',
+            }}
           >
-            Join our Team →
-          </Button>
-        </Link>
-      </Stack>
+            {data.personCollection.items.map((person) => (
+              <Box w={{ base: '20%', sm: '50%', md: '100%' }} mb={{ base: 12, md: 0 }} key={person}>
+                <Image src={person.profilePicture.url} borderRadius="full"/>
+                <Heading fontSize={{ base: '2xl', md: '4xl', xl: '4xl' }} paddingLeft={[0,3,10]}>
+                  {person.position}
+                </Heading>
+                <Text color="gray.500" fontSize="2xl" paddingLeft={[0,3,10]}>
+                  {person.name}
+                </Text>
+              </Box>
+            ))}
+          </SimpleGrid>
+          <Link textDecoration="none" href="/gettinginvolved">
+            <Button
+              backgroundColor="mint.700"
+              color="white"
+              borderRadius="8px"
+              py="4"
+              px="4"
+              lineHeight="1"
+              size="lg"
+              _hover={{ bg: 'mint.300' }}
+            >
+              Join our Team →
+            </Button>
+          </Link>
+        </Stack>  
+      </Flex>
     </Layout>
   );
 };

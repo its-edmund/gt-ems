c16ce864c97b4f1910b60f53edbcf29b5abb41bf
mxinburritos <mxinburritos@gmail.com>
2021-03-16 22:30:57 -0700

    change carsousel styling

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
-import { Flex, Heading, Image, Stack, Text, Box} from '@chakra-ui/react';
+import { Flex, Heading, Image, Stack, Text, Box } from '@chakra-ui/react';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../Layout';
@@ -31,40 +31,67 @@ const QUICK_LINKS_QUERY = gql`
   }
 `;
 
-
 const Resources = () => {
   const { loading, data } = useQuery(QUICK_LINKS_QUERY);
 
   return (
     <Layout>
-      <Flex direction='column' my={10} align='center'>
-        <Heading as='h1' color='mint.700' fontSize='4xl' fontWeight='bold' mb={30}>Resources</Heading>
+      <Flex direction="column" my={10} align="center">
+        <Heading as="h1" color="mint.700" fontSize="4xl" fontWeight="bold" mb={30}>
+          Resources
+        </Heading>
       </Flex>
-      <Carousel>
+      <Carousel style={{ backgroundColor: '#A8DADC' }}>
         {loading ? (
-          <Box mx='auto'><Text>Loading Data...
-          </Text></Box>
+          <Box mx="auto">
+            <Text>Loading Data...</Text>
+          </Box>
         ) : (
           data.usefulVideoCollection.items.map((videoLink, i) => (
             <Carousel.Item key={i}>
-              <Heading className="video_link_title" textAlign="center" fontSize={23} textDecoration="underline" color="blackAlpha.800">{videoLink.title}</Heading>
-              <Text className="video_link_explanation" fontSize={18} textAlign="center" paddingBottom={5} marginLeft="24%" marginRight="24%">{videoLink.explanation}</Text>
-              <ReactPlayer className="d-block w-100" url={videoLink.videoUrl} paddingBottom={5}/>
+              <Box my={10} align="center">
+                <Heading
+                  className="video_link_title"
+                  textAlign="center"
+                  fontSize={23}
+                  textDecoration="underline"
+                  color="blackAlpha.800"
+                >
+                  {videoLink.title}
+                </Heading>
+
+                <Text
+                  className="video_link_explanation"
+                  fontSize={18}
+                  textAlign="center"
+                  paddingBottom={5}
+                  marginLeft="24%"
+                  marginRight="24%"
+                >
+                  {videoLink.explanation}
+                </Text>
+                <Box w="100%">
+                  <ReactPlayer url={videoLink.videoUrl} paddingBottom={5} />
+                </Box>
+              </Box>
             </Carousel.Item>
           ))
         )}
       </Carousel>
-      <Flex direction='column' align='center' paddingTop={10}>
-        <Heading as="h2" mx='auto' my={30} color='mint.700' fontSize='3xl'>Quick Links</Heading>
+      <Flex direction="column" align="center" paddingTop={10}>
+        <Heading as="h2" mx="auto" my={30} color="mint.700" fontSize="3xl">
+          Quick Links
+        </Heading>
         <Stack width={{ base: '80%', md: '60%' }} spacing={20}>
           {loading ? (
-            <Box mx='auto'><Text>Loading Data...
-            </Text></Box>
+            <Box mx="auto">
+              <Text>Loading Data...</Text>
+            </Box>
           ) : (
             data.quickLinkCollection.items.map((quickLink, i) => (
-              <Flex align={{base: 'center'}} direction={{base: 'column', md: 'row'}} key={i}>
-                <Flex direction='column' mr={20}>
-                  <Heading as='h3' size='lg' color='mint.700'>
+              <Flex align={{ base: 'center' }} direction={{ base: 'column', md: 'row' }} key={i}>
+                <Flex direction="column" mr={20}>
+                  <Heading as="h3" size="lg" color="mint.700">
                     <a
                       style={{ textDecoration: 'none' }}
                       href={quickLink.url}
@@ -74,11 +101,11 @@ const Resources = () => {
                       {quickLink.title}
                     </a>
                   </Heading>
-                  <Text color='mint.500'>{quickLink.description}</Text>
+                  <Text color="mint.500">{quickLink.description}</Text>
                 </Flex>
                 <Image
-                  mt={{base: 30, md: 0}}
-                  maxWidth={{base: '80%', md: '30%'}}
+                  mt={{ base: 30, md: 0 }}
+                  maxWidth={{ base: '80%', md: '30%' }}
                   alt={quickLink.logo.description}
                   src={quickLink.logo.url}
                 />
